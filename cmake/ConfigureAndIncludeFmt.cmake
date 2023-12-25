6c8cf60bcfedeaac0b8142440dd53f2da7004a5d
Octavian Dima <3403191+WopsS@users.noreply.github.com>
2023-12-26 01:17:29 +0200

    Fix fmtlib folder when project is not top level

--- cmake/ConfigureAndIncludeFmt.cmake
+++ cmake/ConfigureAndIncludeFmt.cmake
@@ -1,22 +1,25 @@
 option(FMT_INSTALL "" ON)
 
 add_subdirectory(deps/fmt)
-set_target_properties(fmt PROPERTIES FOLDER "Dependencies")
+
+if(PROJECT_IS_TOP_LEVEL)
+  set_target_properties(fmt PROPERTIES FOLDER "Dependencies")
+endif()
 
 mark_as_advanced(
-    FMT_CMAKE_DIR
-    FMT_CUDA_TEST
-    FMT_DEBUG_POSTFIX
-    FMT_DOC
-    FMT_FUZZ
-    FMT_INC_DIR
-    FMT_INSTALL
-    FMT_LIB_DIR
-    FMT_MODULE
-    FMT_OS
-    FMT_PEDANTIC
-    FMT_PKGCONFIG_DIR
-    FMT_SYSTEM_HEADERS
-    FMT_TEST
-    FMT_WERROR
+  FMT_CMAKE_DIR
+  FMT_CUDA_TEST
+  FMT_DEBUG_POSTFIX
+  FMT_DOC
+  FMT_FUZZ
+  FMT_INC_DIR
+  FMT_INSTALL
+  FMT_LIB_DIR
+  FMT_MODULE
+  FMT_OS
+  FMT_PEDANTIC
+  FMT_PKGCONFIG_DIR
+  FMT_SYSTEM_HEADERS
+  FMT_TEST
+  FMT_WERROR
 )
